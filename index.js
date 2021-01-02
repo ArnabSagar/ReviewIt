7ccbf037e4a4b08eb284a82e6c929512068ba09a
Arnab Sagar <arnab.sagar47@gmail.com>
2021-01-01 21:34:34 -0500

    added nodemon

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const path = require('path')
 
 
 const app = express();
 
+
+app.get('/', (req, res) => {
+
+    res.send('<h1>API is being set up</h1>');
+
+});
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log('Server set up'));
-
 
