85cc9160bc7671dd46b5d248946a2a56d909be5b
Arnab Sagar <arnab.sagar47@gmail.com>
2021-01-11 23:57:41 -0500

    added date

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,15 @@
-import './App.css';
-import Review from './components/review/review';
-import Navbar from './components/navbar/navbar';
-
+import "./App.css";
+import Review from "./components/review/review";
+import Navbar from "./components/navbar/navbar";
 
 function App() {
-
-  const title = "Movie title"
+  const title = "Movie title";
 
   return (
     <div className="App">
-
       <main>
         <Navbar />
-        <Review title={title}/>
+        <Review title={title} />
       </main>
     </div>
   );
--- client/src/components/review/review.js
+++ client/src/components/review/review.js
@@ -1,23 +1,21 @@
-import React from 'react'
-
+import React from "react";
 
 const Review = () => {
-    return (
-      <div className="review-component">
-        <h1>Review Component</h1>
-        <div className="review-wrapper">
-          <div className="review-title">
-            {/* <h1>{this.state.title}</h1> */}
-          </div>
+  return (
+    <div className="review-component">
+      <h1>Review Component</h1>
+      <div className="review-wrapper">
+        <div className="review-title">{/* <h1>{this.state.title}</h1> */}</div>
 
-          <div className="review-main">
-            <h2>Movie Name</h2>
-            <h2>Rating</h2>
-            <h2>Description</h2>
-          </div>
+        <div className="review-main">
+          <h2>Movie Name</h2>
+          <h2>Date</h2>
+          <h2>Rating</h2>
+          <h2>Description</h2>
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
-export default Review
+export default Review;
